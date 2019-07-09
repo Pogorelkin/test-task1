62c2daf6729600b146a17303c3ea33a35cbc5784
Vadim <warfirestb@mail.ru>
2019-07-09 16:41:12 +0400

    init branch commit

---
+++ src/main/java/com/epam/Utils/Utils.java
@@ -0,0 +1,12 @@
+package com.epam.Utils;
+
+public class Utils {
+    public String concatenateWords(String string1, String string2) {
+            return string1 + string2;
+
+    }
+
+    public Integer computeFactorial(int number){
+        return 8; //for init
+    }
+}
